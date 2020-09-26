b99fab53a99e5349b5fa53fd831f0e0d57a31ebe
Nourane Elwazane <nouraneelwazane@gmail.com>
2020-09-26 05:24:10 +0200

    action movies loaded dynamically, fix h2

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@
 
     var cinephile = {};
 
+    /**home page links**/
     var index_content_header = "snippets/index-header2.html";
     var index_movie_genres = "snippets/index-movie-genres.html";
-    var allCategoriesUrl = "data/movie-genres.json"
+    var movie_genres_url = "data/movie-genres.json";
+    /**genre links**/
+    var allCategoriesUrl = "data/";
+    var categoryHtml = "snippets/genre-movies-list.html";
+    var categoriesTitleHtml = "snippets/genre-header2.html";
 
     // Convenience function for inserting innerHTML for 'select'
     var insertHtml = function (selector, html) {
@@ -36,10 +41,71 @@
     });
 
     // Load the menu categories view
-   loadIndexCategories = function () {
+    cinephile.loadGenreMovies= function (name, short_name) {
         showLoading("#main-content");
+        allCategoriesUrl +=short_name;
+        allCategoriesUrl +=".json"
         $ajaxUtils.sendGetRequest(
             allCategoriesUrl,
+            buildAndShowCategoriesHTML);
+    };
+    // Builds HTML for the categories page based on the data
+    // from the server
+    function buildAndShowCategoriesHTML (categories) {
+        // Load title snippet of categories page
+        $ajaxUtils.sendGetRequest(
+            categoriesTitleHtml,
+            function (categoriesTitleHtml) {
+                // Retrieve single category snippet
+                $ajaxUtils.sendGetRequest(
+                    categoryHtml,
+                    function (categoryHtml) {
+                        var categoriesViewHtml =
+                            buildCategoriesViewHtml(categories,
+                                                    categoriesTitleHtml,
+                                                    categoryHtml);
+                        insertHtml("#main-content", categoriesViewHtml);
+                    },
+                    false);
+            },
+            false);
+    }
+
+
+    // Using categories data and snippets html
+    // build categories view HTML to be inserted into page
+    function buildCategoriesViewHtml(categories,
+                                      categoriesTitleHtml,
+                                      categoryHtml) {
+
+        var finalHtml = categoriesTitleHtml;
+        finalHtml += "<section class='row'>";
+
+        // Loop over categories
+        for (var i = 0; i < categories.length; i++) {
+            // Insert category values
+            var html = categoryHtml;
+            var name = "" + categories[i].name;
+            var short_name = categories[i].short_name;
+            html =
+                insertProperty(html, "name", name);
+            html =
+                insertProperty(html,
+                               "short_name",
+                               short_name);
+            finalHtml += html;
+        }
+
+        finalHtml += "</section>";
+        return finalHtml;
+    }
+
+
+    // Load the menu categories view
+    loadIndexCategories = function () {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            movie_genres_url,
             buildAndShowMovieGenresHTML);
     };
 
@@ -57,8 +123,8 @@
                     function (index_movie_genre) {
                         var categoriesViewHtml =
                             buildMovieGenresViewHtml(categories,
-                                                    index_content_header,
-                                                    index_movie_genre);
+                                                     index_content_header,
+                                                     index_movie_genre);
                         insertHtml("#main-content", categoriesViewHtml);
                     },
                     false);
@@ -70,8 +136,8 @@
     // Using categories data and snippets html
     // build categories view HTML to be inserted into page
     function buildMovieGenresViewHtml(categories,
-                                      index_content_header,
-                                      index_movie_genre) {
+                                       index_content_header,
+                                       index_movie_genre) {
 
         var finalHtml = index_content_header;
         finalHtml += "<div id='movie-genres' class='row'>";
